cc0a1054d81d678345083d2340665e5ea8f26b0a
squircle12 <leigh@gwyncon.com>
2026-03-26 13:19:44 +0000

    Added Wrexham GUIDs for Setup Scripts

---
+++ BU Import Scripts/Wrexham.sql
@@ -0,0 +1,16 @@
+INSERT INTO [dbo].[LA_BU] ([businessunit], [BU_Name], [Organisation])
+VALUES
+('2101B39F-DEFF-E911-80F7-0050569C3025', LEFT('Wrexham Social Care',100),'Wrexham')
+, ('B9F09955-DFFF-E911-80F7-0050569C3025', LEFT('Wrexham Safeguarding People',100),'Wrexham')
+, ('43FB1E75-DFFF-E911-80F7-0050569C3025', LEFT('Wrexham Adoption',100),'Wrexham')
+, ('474448A0-DFFF-E911-80F7-0050569C3025', LEFT('Wrexham TAC',100),'Wrexham')
+, ('875E96B2-DFFF-E911-80F7-0050569C3025', LEFT('Wrexham Secure Adult',100),'Wrexham')
+, ('5DEBABC5-DFFF-E911-80F7-0050569C3025', LEFT('Wrexham Secure Child',100),'Wrexham')
+, ('729785CA-DEFF-E911-80F7-0050569C3025', LEFT('Wrexham Adult Finance Only',100),'Wrexham')
+, ('616097EA-DEFF-E911-80F7-0050569C3025', LEFT('Wrexham DOLS',100),'Wrexham')
+, ('EB890703-DFFF-E911-80F7-0050569C3025', LEFT('Wrexham Adults',100),'Wrexham')
+, ('FB029A15-DFFF-E911-80F7-0050569C3025', LEFT('Wrexham Disability Team',100),'Wrexham')
+, ('61CCFA2F-DFFF-E911-80F7-0050569C3025', LEFT('Wrexham Children',100),'Wrexham')
+
+GO
+
