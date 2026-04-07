15353019f4fa2035f8637e5b04f2d1c5a0c37d62
squircle12 <leigh@gwyncon.com>
2026-04-07 11:50:32 +0100

    Added Merthyr Scripts

---
+++ BU Import Scripts/Merthyr.sql
@@ -0,0 +1,12 @@
+INSERT INTO [dbo].[LA_BU] ([businessunit], [BU_Name], [Organisation])
+VALUES
+('E05D50FE-275C-E711-80DD-0050569C3025', LEFT('Merthyr - Adults',100),'Merthyr')
+, ('3269E804-285C-E711-80DD-0050569C3025', LEFT('Merthyr - Childrens',100),'Merthyr')
+, ('8F3A660B-285C-E711-80DD-0050569C3025', LEFT('Merthyr - CMHT',100),'Merthyr')
+, ('85947913-285C-E711-80DD-0050569C3025', LEFT('Merthyr - Read Only',100),'Merthyr')
+, ('6065C3B2-1B5F-E711-80DB-0050569C340A', LEFT('Merthyr - Secure Individual Records',100),'Merthyr')
+, ('F8389F42-1B5F-E711-80DB-0050569C340A', LEFT('Merthyr - Secure Safeguarding',100),'Merthyr')
+, ('0167B2CF-275C-E711-80DD-0050569C3025', LEFT('Merthyr - Social Care',100),'Merthyr')
+
+GO
+
